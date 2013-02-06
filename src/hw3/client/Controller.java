7a5ee4f6430c0a4bcb8ccf67a34bdeb88e0b5b7f
alfredo <alfredo@alfredo-Aspire-2930>
2013-02-06 23:59:58 +0100

    auth

--- src/hw3/client/Controller.java
+++ src/hw3/client/Controller.java
@@ -5,7 +5,9 @@
 package hw3.client;
 
 import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.GenericType;
 import com.sun.jersey.api.client.WebResource;
+import hw3.client.gui.serverclasses.Route;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -17,6 +19,7 @@ public class Controller {
     private Client c;
     private String token = null;
     private static Controller instance = null;
+    private Route route = null;
 
     private Controller() {
         c = Client.create();
@@ -40,5 +43,24 @@ public class Controller {
         } catch (Exception e) {
             throw new Exception(e);
         }
+    }
+
+    public void findByItinerary(String from, String to, String date) {
+        String resString = "http://localhost:8080/id2208-hw3-server/webresources/entities.myflight/find_flight_by_itinerary/";
+        resString += from + "/" + to + "/" + date + "/" + token;
+        WebResource r = c.resource(resString);
+
+
+
+        GenericType<Route> genericType = new GenericType<Route>() {};
+        route = r.accept(MediaType.APPLICATION_XML).get(genericType);
+
+    }
+
+    public static void main(String args[]) throws Exception {
+        Controller c = Controller.getInstance();
+        c.authenticate("alfredo", "1234");
+        c.findByItinerary("Barcelona", "Stockholm", "05022013");
+
     }
 }
